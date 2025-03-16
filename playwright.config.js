94350e221a7472729e55e77ba43c4e92caacbd70
snehaprakas1h <snehakp121@gmail.com>
2025-03-16 14:21:48 +0100

    feat: Add allure history to the alure report

--- playwright.config.js
+++ playwright.config.js
@@ -4,31 +4,33 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
-    use: {
-        baseURL: 'https://the-internet.herokuapp.com', // Configurable Base URL
-        headless: true, // Run tests in headless mode
-        screenshot: 'only-on-failure', // Capture screenshot on failure
-        video: 'retain-on-failure', // Capture video on failure
-        launchOptions: {
-            args: ['--start-maximized'], // For full screen
-        },
+  testDir: './tests',
+  use: {
+    baseURL: 'https://the-internet.herokuapp.com', // Configurable Base URL
+    headless: true, // Run tests in headless mode
+    screenshot: 'only-on-failure', // Capture screenshot on failure
+    video: 'retain-on-failure', // Capture video on failure
+    trace: 'on',
+    launchOptions: {
+      args: ['--start-maximized'], // For full screen
     },
-    reporter: [
-        ['line'],
-        ['allure-playwright', { resultsDir: 'allure-results' }],
-        ['html', { outputFolder: 'playwright-report', open: 'never' }],
-    ],
+  },
+  reporter: [
+    ['line'],
+    ['allure-playwright', { resultsDir: 'allure-results' }],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+  ],
 
-    projects: [
-        {
-          name: "chromium",
-          use: {
-            // 2 (Make sure device is not set)
-            // ...devices["Desktop Chrome"],
-    
-            // 3
-            viewport: null, //for full view
-          },
-        },
-      ],
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        // 2 (Make sure device is not set)
+        // ...devices["Desktop Chrome"],
+
+        // 3
+        viewport: null, //for full view
+      },
+    },
+  ],
 });
